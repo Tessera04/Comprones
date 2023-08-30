6e82c3eb65ce3e8ed7bf303bee67fbcc4ac73394
Matias <matute_11@hotmail.com.ar>
2023-08-30 09:29:48 -0300

    Agrego datos en js

---
+++ shop-cart.js
@@ -0,0 +1,10 @@
+let products = [{
+    price: 500,
+    nombre: "Casco de Moto pinguito",
+    cuotas: "6x " + price/12
+},
+{
+    price: 2000,
+    nombre: "Casco de Moto jijero",
+    cuotas: "6x " + price/12
+}]
